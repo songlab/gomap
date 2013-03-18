ceb5fec5639f424d25ef57ceffdd2d7ea903a81f
Aaron Diaz <aad1974@gmail.com>
2013-03-18 12:26:56 -0700

    added code to generate test lists and a few lists themselves

--- generate_test_lists.m
+++ generate_test_lists.m
@@ -1,18 +1,18 @@
 %script to generate example gene lists to use with GO-map
-load ~/research/fun_genom/data/gene_id_maps/refseq2entrez_hsa.mat
+if ~exist('ref2ent_hsa','var'),load ~/research/fun_genom/data/gene_id_maps/refseq2entrez_hsa.mat;end
 refs=ref2ent_hsa.keys;
-load ~/research/fun_genom/data/gene_id_maps/entrez2gsymb_hsa.mat
+if ~exist('ent2symb_hsa','var'),load ~/research/fun_genom/data/gene_id_maps/entrez2gsymb_hsa.mat;end
 ents=ent2symb_hsa.keys;
-load ~/research/fun_genom/data/gene_id_maps/symb2entrez_hsa.mat
+if ~exist('symb2ent_hsa','var'),load ~/research/fun_genom/data/gene_id_maps/symb2entrez_hsa.mat;end
 gsymbs=symb2ent_hsa.keys;
 
 %entrez ids
 f=fopen('example_gene_list_human_entrez_ids.txt','w');
 idx=randi(length(ents),100,1);
 for i=1:100
-    fprintf(f,'%n\t',ents{idx(i)});
-    fprintf(f,'%n\t',2*rand(1)-1);
-    fprintf(f,'%n\n',rand(1));
+    fprintf(f,'%i\t',ents{idx(i)});
+    fprintf(f,'%g\t',2*rand(1)-1);
+    fprintf(f,'%g\n',rand(1));
 end
 fclose(f)
 
@@ -21,17 +21,17 @@ f=fopen('example_gene_list_human_refseq_ids.txt','w');
 idx=randi(length(refs),100,1);
 for i=1:100
     fprintf(f,'%s\t',refs{idx(i)});
-    fprintf(f,'%n\t',2*rand(1)-1);
-    fprintf(f,'%n\n',rand(1));
+    fprintf(f,'%g\t',2*rand(1)-1);
+    fprintf(f,'%g\n',rand(1));
 end
 fclose(f)
 
 %gene symbols ids
-f=fopen('example_gene_list_human_refseq_ids.txt','w');
+f=fopen('example_gene_list_human_gene_symbols.txt','w');
 idx=randi(length(gsymbs),100,1);
 for i=1:100
     fprintf(f,'%s\t',gsymbs{idx(i)});
-    fprintf(f,'%n\t',2*rand(1)-1);
-    fprintf(f,'%n\n',rand(1));
+    fprintf(f,'%g\t',2*rand(1)-1);
+    fprintf(f,'%g\n',rand(1));
 end
 fclose(f)
